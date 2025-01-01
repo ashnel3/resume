6aa307fe4dd66c17f403c52f6d1deb457359088a
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2025-01-01 14:29:07 -0800

    refactor: rename content.json & fix hot-reloading
    - chore add context.example.json
    - refactor allow optional content
    - refactor rename content -> context.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
-import { readFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
 
 // plugins
 import { ViteEjsPlugin } from 'vite-plugin-ejs'
 import { viteSingleFile as ViteSingleFile } from 'vite-plugin-singlefile'
 
-// configuration
-export const EJSContext = async (path: string) => {
-  return JSON.parse((await readFile(path)).toString())
-}
-
 export default defineConfig(async () => {
-  const context = await EJSContext(resolve('./content.json'))
   return {
     root: './src',
     build: {
@@ -20,6 +12,6 @@ export default defineConfig(async () => {
       emptyOutDir: true,
       outDir: '../dist',
     },
-    plugins: [ViteSingleFile(), ViteEjsPlugin(context)],
+    plugins: [ViteSingleFile(), ViteEjsPlugin(await import('./context.json'))],
   }
 })
