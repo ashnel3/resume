85070beac76b30521862e0db5008bcb845a07ccc
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2025-08-07 23:12:20 -0700

    fix: print margins

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import '$styles'
+
+window.addEventListener('beforeprint', () => {
+  console.log('before print')
+  document.body.dataset.print = 'true'
+})
+
+window.addEventListener('afterprint', () => {
+  document.body.dataset.print = 'false'
+})
